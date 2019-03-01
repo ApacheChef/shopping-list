5fd096ec282f49e21a6255d4b0bface67e0d79d9
Christopher Williams <cw@chriswillia.ms>
2019-02-28 18:21:32 -0600

    handle input that is empty or nothing but spaces

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function processListEvents() {
+    // If the input text box is not empty or full of spaces, append the
+    // shopping list with the new item and then clear input text box
     $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
-        newItemInput = $(this).find('#shopping-list-entry');
+        newItem = $(this).find('#shopping-list-entry');
+        //if($(newItem).val() === '' || / +/gi.test($(newItem).val())) {
+        if($(newItem).val() === '' || $(newItem).val().trim() === '') {
+            $(newItem).val('');
+            return;
+        }
         $('.shopping-list').append(
             `<li>
-                <span class="shopping-item">${$(newItemInput).val()}</span>
+                <span class="shopping-item">${$(newItem).val()}</span>
                 <div class="shopping-item-controls">
                     <button class="shopping-item-toggle">
                         <span class="button-label">check</span>
@@ -15,8 +22,15 @@ function processListEvents() {
                 </div>
             </li>`
         );
-        $(newItemInput).val('');
+        $(newItem).val('');
     });
+
+    // Handle check button events on the individual list items, applying the
+    // .shopping-item__checked class to the span element in the parent list item
+    // using event delegation
+
+    // Handle delete button events on the list items, using .remove() to remove
+    // the parent list item using event delegation
 }
 
 $(processListEvents);
